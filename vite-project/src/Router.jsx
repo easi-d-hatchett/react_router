8611a1848f7f144a749391d6745fe1af4ff9575c
Easi-d-Hatchett <easi.hatchett3@gmail.com>
2024-02-27 11:48:54 -0500

    complete

---
+++ vite-project/src/Router.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router-dom";
+import Blue from "./Blue";
+import Red from "./Red";
+import Home from "./home";
+
+import Root from "./Root";
+
+// `createBrowserRouter` takes an array of objects
+// each object represents a route (a URL & the element to render at that URL)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/blue",
+        element: <Blue />,
+      },
+      {
+        path: "/red",
+        element: <Red />,
+      },
+    ],
+  },
+]);
+
+export default router;
